7e7b27aad68fd3dc07dcfc3e038bd9f32aebba41
waxchugdagwad <maxrc@uci.edu>
2016-01-04 15:37:42 -0800

    A lot of setup stuff, logging into sessions should work

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'about' => 'about#index'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
