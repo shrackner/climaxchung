e3aab92b96ef3158a6fdb56d200a5f834813e49d
waxchugdagwad <maxrc@uci.edu>
2016-01-09 02:46:27 -0800

    Enabled logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to root_url
+      redirect_to login_url
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    @current_user = nil
+    redirect_to login_url
   end
 end
